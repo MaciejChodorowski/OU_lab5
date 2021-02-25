9e15ed39e014f017160df197790243c588182ab6
MaciejChodorowski <67687070+MaciejChodorowski@users.noreply.github.com>
2021-02-25 10:59:38 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/OU_lab5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_lab5.dir/main.c.obj"
+  "OU_lab5.exe"
+  "OU_lab5.exe.manifest"
+  "OU_lab5.pdb"
+  "libOU_lab5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OU_lab5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
